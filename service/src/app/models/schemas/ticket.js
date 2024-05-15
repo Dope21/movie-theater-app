e8faca3524b208629c33ceef2dd7224356850608
Dope21 <thanasaklimsila@gmail.com>
2024-05-15 11:01:37 +0700

    chore(schema): add timestamp true

--- service/src/app/models/schemas/ticket.js
+++ service/src/app/models/schemas/ticket.js
@@ -2,15 +2,18 @@ import mongoose from 'mongoose'
 
 const ticketSeatSchema = new mongoose.Schema({
   seatType: { type: String, required: true },
-  postion: { type: String, required: true },
+  position: { type: String, required: true },
 })
 
 const ticketSchema = new mongoose.Schema({
   userId: { type: mongoose.SchemaTypes.ObjectId, required: true, ref: 'users' },
   showAt: { type: mongoose.SchemaTypes.ObjectId, required: true, ref: 'movieshowtimes' },
   seats: { type: [ticketSeatSchema], required: true },
-  totalSeat: { type: Number, required: true },
+  totalSeats: { type: Number, required: true },
   totalPrices: { type: Number, required: true },
+}, {
+  versionKey: false,
+  timestamps: true,
 })
 
 const TicketModel = mongoose.model('tickets', ticketSchema)
