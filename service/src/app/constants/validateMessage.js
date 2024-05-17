228cb64bc796d4f79cbc9107f4eb6216d2dcf8ed
Dope21 <thanasaklimsila@gmail.com>
2024-05-17 11:14:28 +0700

    feat(validator): create order validator

---
+++ service/src/app/constants/validateMessage.js
@@ -0,0 +1,8 @@
+export default {
+  isDate: 'must be date.',
+  isInt: 'must be only numeric.',
+  isArray: 'must be only array.',
+  isString: 'must be only string.',
+  notEmpty: 'is required.',
+  isBase64: 'id is not the right format.',
+}
---
+++ service/src/app/middlewares/executeValidator.js
@@ -0,0 +1,27 @@
+import { validationResult } from 'express-validator'
+
+import { catchResponse } from '../libs/resErrorHandling'
+
+const errorFormat = ({
+  location, msg, param, value,
+}) => ({
+  value,
+  msg: `${param} ${msg}`,
+  param,
+  location,
+})
+
+const executeValidator = (requestValidator) => async (req, res, next) => {
+  await Promise.all(requestValidator.map((validate) => validate(req, res, () => {})))
+  const errors = validationResult(req).formatWith(errorFormat)
+  console.log(errors)
+  if (errors.isEmpty()) return next()
+
+  return catchResponse({
+    httpStatus: 422,
+    serviceCode: 'UNPROCESSABLE_ENTITY',
+    payload: errors.array({ onlyFirstError: true }),
+  })
+}
+
+export default executeValidator
