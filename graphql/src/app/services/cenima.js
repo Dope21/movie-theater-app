9fc0430387413158bc98ab7749c02881876f8ce6
dope21 <thanasaklimsila@gmail.com>
2024-06-05 13:48:11 +0700

    typo(*): cenima -> cinema

--- graphql/src/app/services/cenima.js
+++ graphql/src/app/services/cenima.js
@@ -1,15 +1,15 @@
 import request from '../libs/request'
-import { URL_CENIMA_SERVICE } from '../config'
+import { URL_CINEMA_SERVICE } from '../config'
 
-const getUserById = (id) => request.get(`${URL_CENIMA_SERVICE}/users/${id}`)
-const getAllMovies = () => request.get(`${URL_CENIMA_SERVICE}/movies`)
-const getMovieById = (id) => request.get(`${URL_CENIMA_SERVICE}/movies/${id}`)
-const getShowDatesByMovieId = (id) => request.get(`${URL_CENIMA_SERVICE}/movies/${id}/showtime/dates`)
-const getShowTimesInAllTheaters = (movieId, date) => request.get(`${URL_CENIMA_SERVICE}/movies/${movieId}/showtime/${date}/theaters`)
-const getTheaterById = (id) => request.get(`${URL_CENIMA_SERVICE}/theaters/${id}`)
-const getAllSeatTypes = () => request.get(`${URL_CENIMA_SERVICE}/seat/types`)
-const getBookedSeatByShowTimeId = (id) => request.get(`${URL_CENIMA_SERVICE}/tickets/booked/${id}`)
-const createOrder = (order) => request.post(`${URL_CENIMA_SERVICE}/tickets/`, order)
+const getUserById = (id) => request.get(`${URL_CINEMA_SERVICE}/users/${id}`)
+const getAllMovies = () => request.get(`${URL_CINEMA_SERVICE}/movies`)
+const getMovieById = (id) => request.get(`${URL_CINEMA_SERVICE}/movies/${id}`)
+const getShowDatesByMovieId = (id) => request.get(`${URL_CINEMA_SERVICE}/movies/${id}/showtime/dates`)
+const getShowTimesInAllTheaters = (movieId, date) => request.get(`${URL_CINEMA_SERVICE}/movies/${movieId}/showtime/${date}/theaters`)
+const getTheaterById = (id) => request.get(`${URL_CINEMA_SERVICE}/theaters/${id}`)
+const getAllSeatTypes = () => request.get(`${URL_CINEMA_SERVICE}/seat/types`)
+const getBookedSeatByShowTimeId = (id) => request.get(`${URL_CINEMA_SERVICE}/tickets/booked/${id}`)
+const createOrder = (order) => request.post(`${URL_CINEMA_SERVICE}/tickets/`, order)
 
 export default {
   getUserById,
