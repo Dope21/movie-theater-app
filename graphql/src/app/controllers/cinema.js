530efa615153403e6c8241500772f0917ba733a6
dope21 <thanasaklimsila@gmail.com>
2024-06-05 14:35:51 +0700

    feat(controllers): create cinema contorller

---
+++ graphql/src/app/controllers/cinema.js
@@ -0,0 +1,84 @@
+import cinemaService from '../services/cinema'
+
+const getUserById = async () => {
+  try {
+    const response = await cinemaService.getUserById()
+    return response.data
+  } catch (error) {
+    return error.message
+  }
+}
+
+const getAllMovies = async () => {
+  try {
+    const response = await cinemaService.getAllMovies()
+    return response.data
+  } catch (error) {
+    return error.message
+  }
+}
+
+const getMovieById = async (id) => {
+  try {
+    const response = await cinemaService.getMovieById(id)
+    return response.data
+  } catch (error) {
+    return error.message
+  }
+}
+
+const getShowDatesByMovieId = async (id) => {
+  try {
+    const responseDates = await cinemaService.getShowDatesByMovieId(id)
+    const showDateList = responseDates.data
+
+    const responseTheaters = await cinemaService.getShowTimesInAllTheaters(id, showDateList[0])
+    const intialTheaterList = responseTheaters.data
+
+    return { showDateList, intialTheaterList }
+  } catch (error) {
+    return error.message
+  }
+}
+
+const getShowTimesInAllTheaters = async (movieId, date) => {
+  try {
+    const response = await cinemaService.getShowDatesByMovieId(movieId, date)
+    return response.data
+  } catch (error) {
+    return error.message
+  }
+}
+
+const getTheaterSeats = async (theaterId, showTimeId) => {
+  try {
+    const responseTheater = await cinemaService.getTheaterById(theaterId)
+    const responseBookedSeat = await cinemaService.getBookedSeatByShowTimeId(showTimeId)
+
+    const theater = responseTheater.data
+    const bookedSeat = new Set(responseBookedSeat.data)
+
+    return { theater, bookedSeat }
+  } catch (error) {
+    return error.message
+  }
+}
+
+const createOrder = async () => async (order) => {
+  try {
+    const response = await cinemaService.createOrder(order)
+    return response.data
+  } catch (error) {
+    return error.message
+  }
+}
+
+export default {
+  getUserById,
+  getAllMovies,
+  getMovieById,
+  getShowDatesByMovieId,
+  getShowTimesInAllTheaters,
+  getTheaterSeats,
+  createOrder,
+}
