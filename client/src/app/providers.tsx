f62c3269c0a60968b7c489233c00cefc0c531553
dope21 <thanasaklimsila@gmail.com>
2024-06-19 10:50:51 +0700

    feat(context): redux store provider

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,11 +1,14 @@
 import ApolloClientProvider from '@/contexts/apollo_client'
 import { AntdRegistry } from '@ant-design/nextjs-registry'
+import ReduxStoreProvider from '@/contexts/redux_store'
 
 const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <AntdRegistry>
       <ApolloClientProvider>
-      {children}
+        <ReduxStoreProvider>
+          {children}
+        </ReduxStoreProvider>
       </ApolloClientProvider>
     </AntdRegistry>
   )
---
+++ client/src/contexts/redux_store.tsx
@@ -0,0 +1,8 @@
+import { Provider } from "react-redux"
+import { store } from '@/stores'
+
+const ReduxStoreProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>
+}
+
+export default ReduxStoreProvider
