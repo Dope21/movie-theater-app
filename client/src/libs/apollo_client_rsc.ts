e5634600cc95471bfba378a5f169740ccf8b14b3
dope21 <thanasaklimsila@gmail.com>
2024-06-21 16:21:44 +0700

    refactor(libs): remove comments

--- client/src/libs/apollo_client_rsc.ts
+++ client/src/libs/apollo_client_rsc.ts
@@ -1,17 +1,3 @@
-// import { HttpLink } from '@apollo/client'
-// import { registerApolloClient, ApolloClient, InMemoryCache } from '@apollo/experimental-nextjs-app-support/'
-// import configs from '@/configs'
-
-// export const { getClient } = registerApolloClient(() => {
-//   const httpLink = new HttpLink({
-//     uri: `${configs.API_GATEWAY_URL}/graphql`,
-//   })
-
-//   return new ApolloClient({
-//     cache: new InMemoryCache(),
-//     link: httpLink,
-//   })
-// })
 import { HttpLink } from '@apollo/client'
 import { registerApolloClient, ApolloClient, InMemoryCache } from '@apollo/experimental-nextjs-app-support'
 import configs from '@/configs'
