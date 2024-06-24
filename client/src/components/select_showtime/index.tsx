1a724d33327bec4fb2f5c4b75935f43c59cf5314
dope21 <thanasaklimsila@gmail.com>
2024-06-24 11:54:33 +0700

    feat(component): handle null data

--- client/src/components/select_showtime/index.tsx
+++ client/src/components/select_showtime/index.tsx
@@ -5,6 +5,8 @@ import TheaterList from '@/components/select_showtime/theater_list'
 const SelectShowtime: React.FC<{ movieId: string }> = ({ movieId }) => {
   const { datesData, theaterData, fetchNewTheaterListByDate } = useShowtimes(movieId)
 
+  if (!datesData.data.getShowDatesByMovieId.data) return 'There are no showtimes'
+
   const theaterList = 
     theaterData?.data?.getShowTimesInAllTheaters.data ||
     datesData.data.getShowDatesByMovieId.data.initialTheaterList
