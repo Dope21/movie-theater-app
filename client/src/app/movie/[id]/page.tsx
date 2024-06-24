2b5a9eb5e29cd3858c1b97731d17d110c2543194
dope21 <thanasaklimsila@gmail.com>
2024-06-24 11:49:52 +0700

    fix(app): revert to getclient

--- client/src/app/movie/[id]/page.tsx
+++ client/src/app/movie/[id]/page.tsx
@@ -1,17 +1,16 @@
-'use client'
-
-import { useSuspenseQuery } from '@apollo/client'
 import { Flex } from 'antd'
 import { GET_MOVIE_BY_ID, GetMovieByIdResponse } from './types'
 import MovieCardWithDetail from '@/components/movie_card_with_detail'
 import MoviePreviewPlayer from '@/components/movie_preview_player'
+import { getClient } from '@/libs/apollo_client_rsc'
 
 interface MovieDeatilProps {
   params: { id: string }
 }
 
-const MovieDetail: React.FC<MovieDeatilProps> = ({ params }) => {
-  const { data } = useSuspenseQuery<GetMovieByIdResponse>(GET_MOVIE_BY_ID, { variables: { id: params.id } })
+const MovieDetail: React.FC<MovieDeatilProps> = async ({ params }) => {
+  const client = getClient()
+  const { data } = await client.query<GetMovieByIdResponse>({query: GET_MOVIE_BY_ID,  variables: { id: params.id } })
   const movie = data.getMovieById.data
 
   return (
